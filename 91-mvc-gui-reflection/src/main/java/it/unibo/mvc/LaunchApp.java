74c2ac2a27a51b6c58b9cd42f82f8bd635728fad
Kimi Osti <kimi.osti@studio.unibo.it>
2023-11-28 18:46:51 +0100

    Added reflection to add interfaces

--- 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 91-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,10 +1,11 @@
 package it.unibo.mvc;
 
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberNewControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberStdoutView;
-import it.unibo.mvc.view.DrawNumberSwingView;
+import java.lang.reflect.*;
+import java.util.List;
 
 /**
  * Application entry-point.
@@ -18,17 +19,29 @@ public final class LaunchApp {
      *
      * @param args ignored
      * @throws ClassNotFoundException if the fetches class does not exist
+     * @throws SecurityException
      * @throws NoSuchMethodException if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
      * @throws InstantiationException if the constructor throws exceptions
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
-        final var model = new DrawNumberImpl();
-        final DrawNumberController app = new DrawNumberNewControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberStdoutView());
-        app.addView(new DrawNumberSwingView());
-    }
+    public static void main(final String... args) throws 
+        ClassNotFoundException, 
+        InstantiationException, 
+        IllegalAccessException, 
+        IllegalArgumentException, 
+        InvocationTargetException, 
+        NoSuchMethodException, 
+        SecurityException {
+            final var model = new DrawNumberImpl();
+            final DrawNumberController app = new DrawNumberNewControllerImpl(model);
+            for (final String viewType : List.of("SwingView", "StdoutView")) {
+                final Class<?> view = Class.forName("it.unibo.mvc.view.DrawNumber" + viewType);
+                for (int i = 0; i < 3; i++){
+                    final var newView = view.getConstructor().newInstance();
+                    app.addView((DrawNumberView) newView);
+                }
+            }
+        }
 }
