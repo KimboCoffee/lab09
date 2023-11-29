4d8f6c444261bfb993aa7c1c2c7706901ac76b60
Kimi Osti <kimi.osti@studio.unibo.it>
2023-11-29 19:26:46 +0100

    Getter and setter for currentFile added

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,23 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private File currentFile;
+
+    public Controller(final File currentFile) {
+        this.currentFile = currentFile;
+    }
+
+    public void setCurrentFile(final File newFile) {
+        this.currentFile = newFile;
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
 }
