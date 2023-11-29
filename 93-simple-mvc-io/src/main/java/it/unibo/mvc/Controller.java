c107a5444744951933a55f6108e98406f0e47a99
Kimi Osti <kimi.osti@studio.unibo.it>
2023-11-29 19:46:13 +0100

    Added method to write on file

--- 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 93-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,6 +1,10 @@
 package it.unibo.mvc;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 /**
  * Application controller. Performs the I/O.
@@ -23,5 +27,15 @@ public class Controller {
 
     public String getCurrentFilePath() {
         return this.currentFile.getAbsolutePath();
+    }
+
+    public void write (final String toWrite) throws IOException {
+        if (Objects.isNull(toWrite)) {
+            throw new IOException("Cannot write a null string");
+        } else {
+            try (PrintStream ps = new PrintStream(currentFile, StandardCharsets.UTF_8)) {
+                ps.print(toWrite);                
+            }
+        }
     }
 }
