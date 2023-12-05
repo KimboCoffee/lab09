f93734414716a2ffde70081023f5972a6f5209d6
Kimi Osti <kimi.osti@studio.unibo.it>
2023-12-05 18:37:38 +0100

    Added empty main to setup application

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -10,4 +10,11 @@ public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
 
+    /**
+     * Runs the application.
+     * @param args is unused.
+     */
+    public static void main(String[] args) {
+        
+    }
 }
