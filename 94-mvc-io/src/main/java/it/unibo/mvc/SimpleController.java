6bb91a7c2705be5ad3379ad399b9ebc8b469da21
Kimi Osti <kimi.osti@studio.unibo.it>
2023-12-05 17:58:17 +0100

    Added fields and constructor to handle the Controller implementation

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,22 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 
- *
+ * A simple controller that handles I/O services
  */
 public final class SimpleController implements Controller {
 
+    private final List<String> history;
+    private final String nextString;
+
+    /**
+     * Standard constructor for the Controller.
+     * It creates a new empty list and sets the next string as null,
+     */
+    public SimpleController() {
+        this.history = new ArrayList<String>();
+        this.nextString = null;
+    }
 }
