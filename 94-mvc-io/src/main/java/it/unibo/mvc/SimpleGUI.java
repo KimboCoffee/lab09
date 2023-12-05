2e5fe7433590402f82d742e3937af260caf9b22c
Kimi Osti <kimi.osti@studio.unibo.it>
2023-12-05 19:00:12 +0100

    Added constructor to set up the GUI

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,14 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,7 +16,45 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private final static int PROPORTION = 5;
+
     private final JFrame frame = new JFrame();
+    private final Controller printer = new SimpleController();
+
+    /**
+     * Standard constructor that sets up the view.
+     */
+    public SimpleGUI(){
+        final JPanel canvas = new JPanel();
+        frame.add(canvas);
+        canvas.setLayout(new BorderLayout());
+        final JTextField reader = new JTextField("Write here your next string...");
+        canvas.add(reader, BorderLayout.NORTH);
+        final JTextArea historyViewer = new JTextArea("See here the strings you printed...");
+        canvas.add(historyViewer, BorderLayout.CENTER);
+        final JButton print = new JButton("Print");
+        canvas.add(print, BorderLayout.SOUTH);
+        final JButton seeHistory = new JButton("See history");
+        canvas.add(seeHistory, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        print.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printer.setNextString(reader.getText());
+                printer.printString();
+            }
+
+        });
+        seeHistory.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                historyViewer.setText(printer.getPrintHistory().toString());
+            }
+
+        });
+    }
 
     /**
      * Runs the application.
