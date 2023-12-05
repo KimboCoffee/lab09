3fa680450f815c0ba4f67774d5a954f1f36d094b
Kimi Osti <kimi.osti@studio.unibo.it>
2023-12-05 18:22:54 +0100

    Added NOPMD comment to print on stdout

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,13 +1,13 @@
 package it.unibo.mvc;
 
 /**
- * Interface to print Output on stdout
+ * Interface to print Output on stdout.
  */ 
 public interface Controller {
     /**
      * @param toPrint The string that is going to be printed on stdout.
      */
-    public static void print(String toPrint) {
-        System.out.println(toPrint);
+    static void print(String toPrint) {
+        System.out.println(toPrint); //NOPMD: it is asked to print on stdout
     }
 }
