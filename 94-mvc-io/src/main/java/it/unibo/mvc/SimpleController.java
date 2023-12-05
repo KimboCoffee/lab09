3d53fd783bee4ad5ee9761839a1e11a7d721c82c
Kimi Osti <kimi.osti@studio.unibo.it>
2023-12-05 18:23:22 +0100

    setNextString method added

--- 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -4,19 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A simple controller that handles I/O services
+ * A simple controller that handles I/O services.
  */
 public final class SimpleController implements Controller {
 
     private final List<String> history;
-    private final String nextString;
+    private String nextString;
 
     /**
      * Standard constructor for the Controller.
      * It creates a new empty list and sets the next string as null,
      */
     public SimpleController() {
-        this.history = new ArrayList<String>();
+        this.history = new ArrayList<>();
         this.nextString = null;
+    }
+
+    /**
+     * Setter for the next string to print.
+     * @param newString is the new string to be printed.
+     */
+    public void setNextString(final String newString) {
+        if (newString == null) {
+            throw new IllegalArgumentException("String cannot be set to null");
+        } else {
+            this.nextString = newString;
+        }
     }
 }
