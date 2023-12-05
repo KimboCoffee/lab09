4e4f2f563307c4c937c82f6e6288ab9563d7bc19
Kimi Osti <kimi.osti@studio.unibo.it>
2023-12-05 17:57:27 +0100

    Added static print method

--- 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 94-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,13 @@
 package it.unibo.mvc;
 
 /**
- *
- */
+ * Interface to print Output on stdout
+ */ 
 public interface Controller {
-
+    /**
+     * @param toPrint The string that is going to be printed on stdout.
+     */
+    public static void print(String toPrint) {
+        System.out.println(toPrint);
+    }
 }
